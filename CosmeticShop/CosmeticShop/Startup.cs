f30d81040043f96633527525dc34b5551b4a5d3b
hauvx <hauxuan94@gmail.com@gmail.com>
2019-12-08 10:01:29 +0700

    a

--- CosmeticShop/CosmeticShop/Startup.cs
+++ CosmeticShop/CosmeticShop/Startup.cs
@@ -18,7 +18,7 @@ namespace CosmeticShop
         {
             Configuration = configuration;
         }
-
+        // hậu
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
